e699e292ee5fb28710ed44bc2d16191aacdde750
manudev <meerazo7@hotmail.com>
2024-11-29 15:56:24 -0500

    create client side

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-plusplus */
+import React from 'react';
+import '@fontsource/dm-mono';
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
