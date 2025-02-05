beefe2d1fc1ec6ba40b5db25081a6692628a56bc
manudev <meerazo7@hotmail.com>
2025-02-05 10:25:23 -0500

    remove OpenAI API key from config.js for security reasons

--- server/config.js
+++ server/config.js
@@ -3,8 +3,6 @@ export const PORT = 4000;
 export const HOSTNAME = "localhost";
 // jwt
 export const SECRET_KEY = "tu_secreto_super_seguro";
-// openia
-export const OPENAI_API_KEY = "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";
 // mail
 export const EMAIL_USER = "elustondo129@gmail.com"
 export const EMAIL_PASSWORD = "zzlf pzlq vhvx jwjz";
