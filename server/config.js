d377634bae2c24a9aad3aef99d8ecc213555d010
manudev <meerazo7@hotmail.com>
2025-02-05 10:41:33 -0500

    Remove OpenAI API key from code

--- server/config.js
+++ server/config.js
@@ -3,6 +3,8 @@ export const PORT = 4000;
 export const HOSTNAME = "localhost";
 // jwt
 export const SECRET_KEY = "tu_secreto_super_seguro";
+// openia
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 // mail
 export const EMAIL_USER = "elustondo129@gmail.com"
 export const EMAIL_PASSWORD = "zzlf pzlq vhvx jwjz";
