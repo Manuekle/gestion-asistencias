11b87eebab95f4cbf501e700eb001ab283a89ace
manudev <meerazo7@hotmail.com>
2025-02-08 12:27:34 -0500

    Implement role-based access control in Dashboard and add ProtectedRoute component for route protection

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom'; // Solo importamos Navigate para la redirección
+
+function ProtectedRoute({ role, allowedRoles, children }) {
+  // Si el rol del usuario no está permitido, redirige a la página de acceso denegado o login
+  if (!allowedRoles.includes(role)) {
+    return <Navigate to="/access-denied" replace />;
+  }
+
+  // Si el rol es permitido, renderiza los componentes hijos (contenido de la ruta protegida)
+  return children;
+}
+
+export default ProtectedRoute;
--- client/src/layout/Dashboard.jsx
+++ client/src/layout/Dashboard.jsx
@@ -10,9 +10,11 @@ import {
   LibrariesIcon,
   CustomerService01Icon
 } from 'hugeicons-react';
+import { useSelector } from 'react-redux';
 
 import Sidebar, { SidebarItem } from '../components/Sidebar';
 
+import ProtectedRoute from '../components/ProtectedRoute'; // El componente que creamos
 // pages
 import HomePageDashboard from '../pages/dashboard/HomePageDashboard';
 import SchedulePageDashboard from '../pages/dashboard/SchedulePageDashboard';
@@ -27,40 +29,67 @@ function Dashboard() {
   const params = useParams();
   const urlActive = params['*'];
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const role = userInfo.user.usua_rol;
+  // console.log(userInfo.user);
+
   return (
     <div className="flex flex-row w-full bg-[#FAFBFD]">
       <Sidebar>
-        <SidebarItem
-          icon={<Home07Icon size={20} color="#ffffff" variant="stroke" />}
-          text="Inicio"
-          active={!!(urlActive === 'dashboard' || urlActive === '')}
-          to="/dashboard"
-          // alert
-        />
-        <SidebarItem
-          icon={<Calendar03Icon size={20} color="#ffffff" variant="stroke" />}
-          text="Horarios"
-          active={!!(urlActive === 'schedule')}
-          to="/dashboard/schedule"
-        />
-        <SidebarItem
-          icon={<UserGroupIcon size={20} color="#ffffff" variant="stroke" />}
-          text="Clases"
-          active={!!(urlActive === 'class')}
-          to="/dashboard/class"
-        />
-        <SidebarItem
-          icon={<LibrariesIcon size={20} color="#ffffff" variant="stroke" />}
-          text="Asignaturas"
-          active={!!(urlActive === 'signature')}
-          to="/dashboard/signature"
-        />
-        <SidebarItem
-          icon={<NoteIcon size={20} color="#ffffff" variant="stroke" />}
-          text="Reportes"
-          active={!!(urlActive === 'report')}
-          to="/dashboard/report"
-        />
+        {role === 'administrador' || role === 'docente' ? (
+          <>
+            <SidebarItem
+              icon={<Home07Icon size={20} color="#ffffff" variant="stroke" />}
+              text="Inicio"
+              active={!!(urlActive === 'dashboard' || urlActive === '')}
+              to="/dashboard"
+              // alert
+            />
+            <SidebarItem
+              icon={
+                <UserGroupIcon size={20} color="#ffffff" variant="stroke" />
+              }
+              text="Clases"
+              active={!!(urlActive === 'class')}
+              to="/dashboard/class"
+            />
+            {/* <SidebarItem
+              icon={
+                <LibrariesIcon size={20} color="#ffffff" variant="stroke" />
+              }
+              text="Asignaturas"
+              active={!!(urlActive === 'signature')}
+              to="/dashboard/signature"
+            /> */}
+            <SidebarItem
+              icon={<NoteIcon size={20} color="#ffffff" variant="stroke" />}
+              text="Reportes"
+              active={!!(urlActive === 'report')}
+              to="/dashboard/report"
+            />
+          </>
+        ) : null}
+        {role === 'estudiante' ? (
+          <>
+            <SidebarItem
+              icon={<Home07Icon size={20} color="#ffffff" variant="stroke" />}
+              text="Inicio"
+              active={!!(urlActive === 'dashboard' || urlActive === '')}
+              to="/dashboard"
+            />
+            <SidebarItem
+              icon={
+                <Calendar03Icon size={20} color="#ffffff" variant="stroke" />
+              }
+              text="Horarios"
+              active={!!(urlActive === 'schedule')}
+              to="/dashboard/schedule"
+            />
+          </>
+        ) : null}
+
         <hr className="my-3 border-1 border-zinc-800" />
         <SidebarItem
           icon={<Settings03Icon size={20} color="#ffffff" variant="stroke" />}
@@ -77,14 +106,94 @@ function Dashboard() {
       </Sidebar>
       <section className="p-8 w-full bg-[#FAFBFD]">
         <Routes>
-          <Route path="/" element={<HomePageDashboard />} />
-          <Route path="schedule" element={<SchedulePageDashboard />} />
-          <Route path="schedule/:id" element={<Classes />} />
-          <Route path="class" element={<ClassPageDashboard />} />
-          <Route path="class/:name/:id" element={<Classes />} />
-          <Route path="signature" element={<SignaturePageDashboard />} />
-          <Route path="report" element={<ReportPageDashboard />} />
-          <Route path="settings" element={<ConfigPageDashboard />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente', 'estudiante']}
+              >
+                <HomePageDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="schedule"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'estudiante']}
+              >
+                <SchedulePageDashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* <Route
+            path="schedule/:id"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente', 'estudiante']}
+              >
+                <Classes />
+              </ProtectedRoute>
+            }
+          /> */}
+          <Route
+            path="class"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente']}
+              >
+                <ClassPageDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="class/:name/:id"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente']}
+              >
+                <Classes />
+              </ProtectedRoute>
+            }
+          />
+          {/* <Route
+            path="signature"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente']}
+              >
+                <SignaturePageDashboard />
+              </ProtectedRoute>
+            }
+          /> */}
+          <Route
+            path="report"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente']}
+              >
+                <ReportPageDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="settings"
+            element={
+              <ProtectedRoute
+                role={role}
+                allowedRoles={['administrador', 'docente', 'estudiante']}
+              >
+                <ConfigPageDashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </section>
     </div>
