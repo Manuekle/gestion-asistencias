e9f5ea27117079772da083dd8b748bf3cdc37804
manudev <meerazo7@hotmail.com>
2025-02-13 17:20:57 -0500

    Add coverage reports and update Jest configuration for improved test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 export default {
+  testMatch: [
+    "**/tests/**/*.test.js",
+    "**/tests/**/*.spec.js",
+    "**/__tests__/**/*.js",
+  ],
+  testPathIgnorePatterns: ["/node_modules/"],
   testEnvironment: "node",
-  transform: { "^.+\\.jsx?$": "babel-jest" },
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/"],
+  collectCoverage: true,
+  collectCoverageFrom: ["**/src/**/*.{js,jsx}", "!**/node_modules/**"],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  passWithNoTests: true,
 };
