2d9120c4b9eebc14ab4582680fbac4cebf5cf4fb
manudev <meerazo7@hotmail.com>
2025-02-16 17:05:42 -0500

    Load environment variables from .env file for database configuration

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
 import { createPool } from 'mysql2/promise';
+
+dotenv.config();
 
 const pool = createPool({
   host: process.env.BD_HOSTNAME,
   port: process.env.BD_PORT,
   user: process.env.BD_USER,
   database: process.env.BD_NAME,
-  password: '',
+  password: process.env.BD_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
