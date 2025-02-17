589ee04560aee7067d76d770dffce32d60e45296
manudev <meerazo7@hotmail.com>
2025-02-16 23:53:15 -0500

    Refactor SQL queries and improve password recovery logic in usuarios.controller.js

--- server/controllers/usuarios.controller.js
+++ server/controllers/usuarios.controller.js
@@ -1,20 +1,20 @@
 //? Usuarios Controllers
-import { pool } from '../db.js';
-import { createUsuarioSchema } from '../schemas/usuario.js';
+import { pool } from "../db.js";
+import { createUsuarioSchema } from "../schemas/usuario.js";
 
-import { SECRET_KEY, EMAIL_USER, EMAIL_PASSWORD } from '../config.js';
+import { SECRET_KEY, EMAIL_USER, EMAIL_PASSWORD } from "../config.js";
 
-import { z } from 'zod';
-import nodemailer from 'nodemailer';
-import bcrypt from 'bcrypt';
-import crypto from 'crypto';
-import jwt from 'jsonwebtoken';
+import { z } from "zod";
+import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 //* GET
 export const getUsuarios = async (req, res) => {
   try {
     const [result] = await pool.query(
-      'SELECT * FROM usuario ORDER BY created_at ASC'
+      "SELECT * FROM usuario ORDER BY created_at ASC"
     );
     return res.status(200).json(result);
   } catch (error) {
@@ -25,11 +25,11 @@ export const getUsuarios = async (req, res) => {
 export const getUsuario = async (req, res) => {
   try {
     const [result] = await pool.query(
-      'SELECT * FROM usuario WHERE usua_id = ?',
+      "SELECT * FROM usuario WHERE usua_id = ?",
       [req.params.id]
     );
     if (result.length === 0) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
     return res.status(200).json(result[0]);
   } catch (error) {
@@ -44,12 +44,12 @@ export const createUsuario = async (req, res) => {
 
     // Verificar si el correo ya existe
     const [existingUser] = await pool.query(
-      'SELECT * FROM usuario WHERE usua_correo = ?',
+      "SELECT * FROM usuario WHERE usua_correo = ?",
       [data.usua_correo]
     );
 
     if (existingUser.length > 0) {
-      return res.status(400).json({ message: 'El correo ya está registrado' });
+      return res.status(400).json({ message: "El correo ya está registrado" });
     }
 
     // Hash de la contraseña
@@ -57,13 +57,13 @@ export const createUsuario = async (req, res) => {
 
     // Insertar usuario con contraseña cifrada
     const [result] = await pool.query(
-      'INSERT INTO usuario(usua_nombre, usua_correo, usua_password, rol, usua_estado) VALUES (?, ?, ?, ?, ?)',
+      "INSERT INTO usuario(usua_nombre, usua_correo, usua_password, rol, usua_estado) VALUES (?, ?, ?, ?, ?)",
       [
         data.usua_nombre,
         data.usua_correo,
         hashedPassword,
         data.rol,
-        data.usua_estado
+        data.usua_estado,
       ]
     );
 
@@ -71,7 +71,7 @@ export const createUsuario = async (req, res) => {
     const token = jwt.sign(
       { id: result.insertId, correo: data.usua_correo },
       SECRET_KEY,
-      { expiresIn: '1h' } // Duración del token
+      { expiresIn: "1h" } // Duración del token
     );
 
     // show data user and no show user.usua_password;
@@ -80,10 +80,10 @@ export const createUsuario = async (req, res) => {
       usua_nombre: data.usua_nombre,
       usua_correo: data.usua_correo,
       rol: data.rol,
-      usua_estado: data.usua_estado
+      usua_estado: data.usua_estado,
     };
 
-    return res.status(200).json({ message: 'Registro exitoso', token, user });
+    return res.status(200).json({ message: "Registro exitoso", token, user });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return res.status(400).json({ message: error.errors[0].message });
@@ -99,15 +99,15 @@ export const loginUsuario = async (req, res) => {
 
     // Buscar usuario por correo
     const [result] = await pool.query(
-      'SELECT * FROM usuario WHERE usua_correo = ?',
+      "SELECT * FROM usuario WHERE usua_correo = ?",
       [usua_correo]
     );
 
     if (result.length === 0) {
       return res.status(404).json({
-        code: 'NOT_FOUND',
+        code: "NOT_FOUND",
         status: 404,
-        message: 'Usuario no encontrado'
+        message: "Usuario no encontrado",
       });
     }
 
@@ -120,9 +120,9 @@ export const loginUsuario = async (req, res) => {
     );
     if (!isPasswordValid) {
       return res.status(404).json({
-        code: 'NOT_FOUND',
+        code: "NOT_FOUND",
         status: 404,
-        message: 'Contraseña incorrecta'
+        message: "Contraseña incorrecta",
       });
     }
 
@@ -130,7 +130,7 @@ export const loginUsuario = async (req, res) => {
     const token = jwt.sign(
       { id: user.usua_id, correo: user.usua_correo },
       SECRET_KEY,
-      { expiresIn: '1h' } // Token válido por 1 hora
+      { expiresIn: "1h" } // Token válido por 1 hora
     );
 
     // Renombrar las propiedades del usuario
@@ -139,21 +139,21 @@ export const loginUsuario = async (req, res) => {
       user_nombre: user.usua_nombre,
       user_correo: user.usua_correo,
       user_estado: user.usua_estado,
-      rol: user.rol
+      rol: user.rol,
     };
 
     return res.status(200).json({
-      code: 'SUCCESS',
+      code: "SUCCESS",
       status: 200,
-      message: 'Autenticado correctamente',
+      message: "Autenticado correctamente",
       token,
-      user: transformedUser
+      user: transformedUser,
     });
   } catch (error) {
     return res.status(500).json({
-      code: 'INTERNAL_SERVER_ERROR',
+      code: "INTERNAL_SERVER_ERROR",
       status: 500,
-      message: 'Ocurrió un error inesperado'
+      message: "Ocurrió un error inesperado",
     });
   }
 };
@@ -163,45 +163,73 @@ export const recoverPassword = async (req, res) => {
   try {
     const { usua_correo } = req.body;
 
-    // Verificar si el correo existe
-    const [result] = await pool.query(
-      'SELECT * FROM usuario WHERE usua_correo = ?',
+    // Verificar si el correo existe en alguna de las tres tablas
+    let result = await pool.query(
+      "SELECT * FROM usuario WHERE usua_correo = ?",
       [usua_correo]
     );
 
-    if (result.length === 0) {
-      return res.status(404).json({ message: 'Correo no encontrado' });
+    let user = result[0];
+
+    // Si no se encuentra en la tabla 'usuario', buscar en la tabla 'docente'
+    if (!user) {
+      result = await pool.query("SELECT * FROM docente WHERE doc_correo = ?", [
+        usua_correo,
+      ]);
+      user = result[0];
     }
 
-    const user = result[0];
+    // Si no se encuentra en la tabla 'docente', buscar en la tabla 'estudiante'
+    if (!user) {
+      result = await pool.query(
+        "SELECT * FROM estudiante WHERE estu_correo = ?",
+        [usua_correo]
+      );
+      user = result[0];
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: "Correo no encontrado" });
+    }
 
     // Generar una nueva contraseña temporal
-    const newPassword = crypto.randomBytes(8).toString('hex'); // Genera una contraseña aleatoria
+    const newPassword = crypto.randomBytes(8).toString("hex"); // Genera una contraseña aleatoria
     const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-    // Actualizar la contraseña en la base de datos
-    await pool.query('UPDATE usuario SET usua_password = ? WHERE usua_id = ?', [
-      hashedPassword,
-      user.usua_id
-    ]);
+    // Actualizar la contraseña en la base de datos según el tipo de usuario
+    let updateQuery, userId;
+    if (user.usua_id) {
+      updateQuery = "UPDATE usuario SET usua_password = ? WHERE usua_id = ?";
+      userId = user.usua_id;
+    } else if (user.doc_id) {
+      updateQuery = "UPDATE docente SET doc_password = ? WHERE doc_id = ?";
+      userId = user.doc_id;
+    } else if (user.estu_id) {
+      updateQuery = "UPDATE estudiante SET estu_password = ? WHERE estu_id = ?";
+      userId = user.estu_id;
+    }
+
+    await pool.query(updateQuery, [hashedPassword, userId]);
 
     // Configurar el servicio de nodemailer
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
+      service: "gmail",
+      host: "smtp.gmail.com",
       port: 587,
       auth: {
         user: EMAIL_USER,
-        pass: EMAIL_PASSWORD
-      }
+        pass: EMAIL_PASSWORD,
+      },
     });
 
     // Configurar el contenido del correo
     const mailOptions = {
       from: EMAIL_USER,
       to: usua_correo,
-      subject: 'Recuperación de contraseña',
-      text: `Hola ${user.usua_nombre}, Tu nueva contraseña temporal es: ${newPassword}\n\nTe recomendamos cambiarla después de iniciar sesión.`
+      subject: "Recuperación de contraseña",
+      text: `Hola ${
+        user.usua_nombre || user.doc_nombre || user.estu_nombre
+      }, Tu nueva contraseña temporal es: ${newPassword}\n\nTe recomendamos cambiarla después de iniciar sesión.`,
     };
 
     // Enviar el correo
@@ -209,7 +237,7 @@ export const recoverPassword = async (req, res) => {
 
     return res
       .status(200)
-      .json({ message: 'Correo enviado con la nueva contraseña' });
+      .json({ message: "Correo enviado con la nueva contraseña" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
