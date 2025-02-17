c1d02168e542f607e3481f473a80376aa6d6d4ac
manudev <meerazo7@hotmail.com>
2025-02-16 23:06:24 -0500

    Add user role validation to EstudiantePage and redirect non-students

--- client/src/pages/EstudiantePage.jsx
+++ client/src/pages/EstudiantePage.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function EstudiantePage() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, userInfo } = userLogin;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo.user.rol === 'estudiante') {
+      navigate('/');
+    }
+  }, [userInfo, navigate]);
   return (
     <div className="w-full h-screen bg-[#FAFBFD] auth">EstudiantePage</div>
   );
