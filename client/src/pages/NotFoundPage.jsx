061e7285aec2a739bf654acdeef438dd3a149dd1
manudev <meerazo7@hotmail.com>
2025-02-17 10:12:58 -0500

    Enhance NotFoundPage with improved styling and navigation link

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="w-full h-screen bg-[#FAFBFD] auth flex gap-8 flex-col justify-center items-center">
+      <div className="flex flex-col w-full max-w-xl mx-auto px-4 py-16 bg-[#1E201E] rounded-xl shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+        <h1 className="self-center mb-2 text-lg font-light text-zinc-100">
+          Ooops...
+        </h1>
+        <h1 className="self-center mb-2 font-bold text-6xl text-zinc-100">
+          404
+        </h1>
+        <h1 className="self-center mb-2 font-bold text-xl text-zinc-100">
+          Pagina no encontrada
+        </h1>
+        <Link to="/" className="text-xs self-center underline text-amber-400">
+          volver a inicio
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
