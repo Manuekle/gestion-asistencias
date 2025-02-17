2f199481bf07bbb6c7acba0ca0b26d68ea7c4fac
manudev <meerazo7@hotmail.com>
2025-02-17 10:04:23 -0500

    Implement protected routes for student and dashboard pages; add ESLint configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
-/* eslint-disable import/extensions */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-plusplus */
 import React from 'react';
 // Supports weights 100-900
 import '@fontsource-variable/manrope';
-
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // pages
@@ -23,18 +20,45 @@ import LoginPageDocente from './pages/auth/docente/LoginPageDocente.jsx';
 import RegisterPageDocente from './pages/auth/docente/RegisterPageDocente.jsx';
 import RestorePasswordPageAuth from './pages/auth/RestorePasswordPageAuth';
 import NotFoundPage from './pages/NotFoundPage';
+
+// proteger rutas
+import ProtectedRoute from './components/ProtectedRoute';
+
 import { Toaster } from './components/ui/toaster.tsx';
 
 function App() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   return (
     <div className="">
       <Toaster />
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/student" element={<EstudiantePage />} />
+          <Route
+            path="/student"
+            element={
+              <ProtectedRoute
+                role={userInfo && userInfo.user.rol}
+                allowedRoles={['estudiante']}
+              >
+                <EstudiantePage />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/attendance" element={<Attendance />} />
-          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route
+            path="/dashboard/*"
+            element={
+              <ProtectedRoute
+                role={userInfo && userInfo.user.rol}
+                allowedRoles={['administrador', 'docente']}
+              >
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
           {/* admin */}
           <Route
             path="/auth/administrador/login"
